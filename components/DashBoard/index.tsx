60f250d43e7de8a591eddf87a31526e72a987255
Siddharth Soni <soni28siddh@gmail.com>
2023-10-03 14:45:56 +0530

    SS: Added Dashboard page

---
+++ components/DashBoard/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Style from "./index.module.scss";
+
+const Dashboard = () => {
+  return <div className={Style.main}>Dashboard</div>;
+};
+
+export default Dashboard;
