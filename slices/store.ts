46d6a220ff427e4bbc47d19443d376fb0525ff30
Siddharth Soni <soni28siddh@gmail.com>
2023-10-03 14:39:19 +0530

    SS: configure user again also added UseAppSelector and useAppDispatch

--- slices/store.ts
+++ slices/store.ts
@@ -1,6 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./user";
-const store = configureStore({
-  reducer: { rootReducer },
+import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import userReducer from "./user";
+
+export const store = configureStore({
+  reducer: { user: userReducer },
 });
-export default store;
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
