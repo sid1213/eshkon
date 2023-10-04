af93bb20f89d0a22be71c49f92f3fdcd332a2f87
Siddharth Soni <soni28siddh@gmail.com>
2023-10-04 14:36:15 +0530

    SS: Removed unnecessary files

--- components/DashBoard/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Style from "./index.module.scss";
-
-const Dashboard = () => {
-  return <div className={Style.main}>Dashboard</div>;
-};
-
-export default Dashboard;
